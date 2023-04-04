c723ff11446ab9b0ada7d2addcc4de59fabf2b8d
Bhola Kumar <bholakumarmuz67@gmail.com>
2023-04-04 23:24:20 +0530

    add html file

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//function for meter tag
+function increasemeter(){
+    var i = document.getElementById("mtr");
+    i.value+=5;
+}
+
+function decreasemeter(){
+    var i = document.getElementById("mtr");
+    i.value-=5;
+}
+
+//function for progress tag
